3729277f3d8188d2077c6b07f28c36981ec62c44
강다영 <gadin8631@naver.com>
2024-07-29 17:39:30 +0900

    🎸etc: 싸피 -> 집 이동을 위한 임시방편 커밋

--- src/main/java/com/haribo/notification_service/config/RabbitMQConfig.java
+++ src/main/java/com/haribo/notification_service/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.haribo.notification_service.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +16,22 @@ public class RabbitMQConfig {
     @Bean
     public Queue notificationQueue() {
         return new Queue(NOTIFICATION_QUEUE, false);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory("localhost");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
